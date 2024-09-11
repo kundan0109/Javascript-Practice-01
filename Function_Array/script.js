84b710762466fc60cc734b99214236a13016f2d1
Kundan <kundantikale4@gmail.com>
2024-09-11 21:00:23 +0530

    inddex

--- Function_Array/script.js
+++ Function_Array/script.js
@@ -19,9 +19,6 @@
 
 // countVovels("kundan");
 
-
-
-
 // const countVovels = (str) => {
 //   let count = 0;
 //   for (let char of str) {
@@ -41,17 +38,11 @@
 
 // countVovels("abcdefiii");
 
-
 // METHOD IS FUNCTION WHICH IS ASSOCIATED WITH A OBJECT
 
 // METHOD IS A SET OF CODE TO PERFORM A TASK WHICH IS ASSOCIATED WITH A OBJECT or with a data structure.
-// eg.  
+// eg.
 // UpperCase is method associated with the object string .
-
-
-
-
-
 
 // CALLBACK is a function which is pass as a argument to another function.
 
@@ -61,25 +52,17 @@
 //     console.log(val);
 // });
 
-
 // array.forEach((value , index , array) => {
 //     console.log(value , index ,  array);
 // })
 
+// HIGHER ORDER FUNCTION ---->  function which take function as a parameter  or return a function as a value
 
+// ARRAY METHODS
 
- // HIGHER ORDER FUNCTION ---->  function which take function as a parameter  or return a function as a value 
-
-
-
- // ARRAY METHODS 
- 
- // MAP --> it creates new array 
- // filter --> to filter out based on some condition
- // reduce ---> works on accumulator and current(previous and current)
-
-
-
+// MAP --> it creates new array
+// filter --> to filter out based on some condition
+// reduce ---> works on accumulator and current(previous and current)
 
 //  let n = prompt("Enter  a number : ");
 
@@ -88,15 +71,22 @@
 //     arr.push(i);
 //  }
 
-//  console.log(arr);
-//  let sum = arr.reduce((previous , current) => {
-//     return previous + current;
-//  })
+ console.log(arr);
+ let sum = arr.reduce((previous , current) => {
+    return previous + current;
+ })
 
-//  console.log("SO sum is : " + sum);
+ console.log("SO sum is : " + sum);
 
-//  let mul = arr.reduce((previous , current) => {
-//     return previous * current;
-//  })
+ let mul = arr.reduce((previous , current) => {
+    return previous * current;
+ })
 
-//  console.log("SO mul is : " + mul);
+ console.log("SO mul is : " + mul);
+
+
+
+
+
+
+
